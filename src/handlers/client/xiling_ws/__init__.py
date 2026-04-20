abfbf47d0dbdc1d13663ec1b1f165e5d7f1482b0
Leon <happy168@aliyun.com>
2026-04-20 20:16:16 +0800

    updated

--- src/handlers/client/xiling_ws/__init__.py
+++ src/handlers/client/xiling_ws/__init__.py
@@ -3,11 +3,11 @@ Xiling WebSocket Client Handler
 与百度数字人开放平台 WebSocket API 兼容的客户端处理器
 """
 
-from .xiling_ws_handler import XilingWsHandler, XilingWsConfigModel
+from .xiling_ws_handler import XilingWsHandler, XilingWsConfig
 from .xiling_session_delegate import XilingSessionDelegate
 
 __all__ = [
     "XilingWsHandler",
-    "XilingWsConfigModel", 
+    "XilingWsConfig",
     "XilingSessionDelegate",
 ]
